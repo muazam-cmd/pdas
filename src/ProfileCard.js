5edb4ace0059ac169b23777e6fb0a3c2c0850d71
muazam-cmd <muazamshahid7@gmail.com>
2023-01-26 20:16:10 +0400

    using argument destructuring

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,11 @@
-function ProfileCard(props) {
+function ProfileCard({ title, handle }) {
+
+    // const { title, handle } = props;
 
     return (
         <div>
-            <div>Title is {props.title}</div>
-            <div>Handle is {props.handle}</div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
         </div>
     );
 }
